ef9666ec44d9ae6bc3dfb24c0edc634a673f6c65
pinarkirbasoglu <99248927+pinarkirbasoglu@users.noreply.github.com>
2023-05-17 20:27:47 +0300

    Add files via upload

---
+++ WebForm1.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="beni_hatirla_gp.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Kullanıcı adı:<asp:TextBox ID="kuadi" runat="server"></asp:TextBox>
+            <br />
+            Şifre:<asp:TextBox ID="sif" runat="server"></asp:TextBox>
+            <br />
+            <asp:CheckBox ID="CheckBox1" runat="server" Text="beni hatırla" />
+        </div>
+        <asp:Button ID="btn" runat="server" OnClick="btn_Click" Text="giriş" />
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace beni_hatirla_gp
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        HttpCookie cerez = new HttpCookie("hatirla");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.Cookies["hatirla"] != null)
+            {
+                cerez = Request.Cookies["hatirla"];//gelen çerezi alıp bir çerez nesenesine atama atma yapmadan kullanamyız.
+                //sayfa açıldığında çerez varsa kullancı adı textboxında cerezden gelen cerezin kullanici değişikenin değeri yazacak,
+                //aynı şekilde çerez varsa şifre kısmında cerezden gelen sifrem değişkenin değeri yazılı olacak.
+                kuadi.Text = cerez["kullanici"];
+                sif.Text = cerez["sifrem"];
+            }
+
+        }
+
+        protected void btn_Click(object sender, EventArgs e)
+        {
+            string kuladi = kuadi.Text;
+            string sifre = sif.Text;
+            if(kuladi.Equals("butgem")&& sifre.Equals("12345"))
+            {
+                if (CheckBox1.Checked)
+                {
+                    cerez["kullanici"] = kuladi;
+                    cerez["sifrem"] = sifre;
+                    cerez.Expires = DateTime.Now.AddYears(1);
+                    Response.Cookies.Add(cerez);//çerez atma beni hatırla seçili ise yapcak
+                }
+                Response.Redirect("Anasayfa.aspx");
+            }
+            else
+            {
+                Response.Write("kullanıcı adı veya şifreniz hatalı");
+                //hatalı ise şifre kullanıcı adı veya şifreninz hatalı diyip textboxların içlerini boşaltacak.
+                kuadi.Text = " ";
+                sif.Text = " ";
+            }
+        }
+    }
+}
